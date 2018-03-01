589be816158b0c9905f942899200ffeb576a5459
Kevin Boyle <papershack@gmail.com>
2018-03-01 14:29:27 -0800

    add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from './../assets/Sieve_Jobs.png';
 
 function Header() {
   return(
@@ -7,14 +8,21 @@ function Header() {
       <style jsx>{`
           h1 {
             text-align: center;
-            font-family: 'Saira Condensed', sans-serif;
+            font-family: 'Roboto Slab', serif;
+            font-weight: 100;
             }
           .links {
             display: flex;
             justify-content: space-around;
           }
+          .header {
+            display: flex;
+          }
         }`}</style>
-      <h1>Sieve Jobs</h1>
+      <div className="header">
+        <img src={logo} alt='logo'/>
+        <h1>SIEVE JOBS</h1>
+      </div>
       <div className="links">
         <Link to="/">Home</Link> | <Link to="/flashcards">Flashcards</Link> | <Link to="/tutorial">Tutorial</Link> | <Link to="/edit">Add/Edit</Link>
       </div>
