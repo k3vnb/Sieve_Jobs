7aa807623bb4fdad1224bac341d979a4d2f0865f
Kevin Boyle <papershack@gmail.com>
2018-03-02 13:12:02 -0800

    add note & branch test

--- src/components/FlashcardControl.jsx
+++ src/components/FlashcardControl.jsx
@@ -9,6 +9,9 @@ function FlashcardControl(){
       <div>
         <h4>Flashcard back side (will re-render static card info & user input info)</h4>
       </div>
+      <div>
+        <button>Click here to go to next flashcard</button>
+      </div>
     </div>
   );
 }
@@ -17,3 +20,5 @@ function FlashcardControl(){
 export default FlashcardControl;
 
 //At this point, just a functional component, it will be refactored to contain state and display flashcard components (front & backside info based on re-usable card info & user-input)
+
+//I believe I will use conditional rendering with a toggle event, if boolean value is true it will render the front of the card, if false it will render the back.
