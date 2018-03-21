0cd9293a0fd6e6c5a20a84bce87e47f3a95980c2
Kevin Boyle <papershack@gmail.com>
2018-03-20 20:19:51 -0700

    add favicon

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import favicon from './assets/favicon.png';
+import Favicon from 'react-favicon';
 import { AppContainer } from 'react-hot-loader';
 import { HashRouter } from 'react-router-dom';
 
 const render = (Component) => {
   ReactDOM.render(
+    <div>
+    <Favicon url={favicon}/>
     <AppContainer>
       <HashRouter>
         <Component/>
       </HashRouter>
-    </AppContainer>,
+    </AppContainer>
+    </div>,
     document.getElementById('react-app-root')
   );
 };
